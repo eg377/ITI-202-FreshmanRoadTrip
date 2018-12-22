2a0b5ed00dd744c8c176536b9224c1855d4e1e75
eg377 <eg377@rutgers.edu>
2018-12-21 21:58:18 -0500

    Update bar.java

--- bar.java
+++ bar.java
@@ -190,8 +190,8 @@ public class bar {
 			int randomInt1 = random1.nextInt((9 - 0) + 1);
 			if(randomInt1<4){
 				System.out.println("\nCongrats. You made it to your second class!");
-				currentLocation="Cook/Douglass"
-				currentStop="Douglass Student Center"
+				currentLocation="Cook/Douglass";
+				currentStop="Douglass Student Center";
 				classes +=1;
 				nextMenu();
 				randomItem();
@@ -243,8 +243,8 @@ public class bar {
 			int randomInt2 = random2.nextInt((9 - 0) + 1);
 			if(randomInt2<4){
 				System.out.println("\nCongrats. You made it to your third and last class!");
-				currentLocation="Busch"
-				currentStop="Hill Center"
+				currentLocation="Busch";
+				currentStop="Hill Center";
 				classes+=1;
 				System.out.println(classes);
 				nextMenu();
